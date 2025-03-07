4a8a18e6480d72a6693ab34f6ae046084b272a9e
DAVIDHCA3 <dscastelblanco82@ucatolica.edu.co>
2025-03-06 22:15:54 -0500

    F faltante 8

--- src/data/data.py
+++ src/data/data.py
@@ -50,8 +50,8 @@ class Data:
         resultado = []
         for elem in lista:
             if elem not in resultado:
-                resultado.append(elem)
-        return resultado 
+            resultado.append(elem)
+    return resultado 
     
     def merge_ordenado(self, lista1, lista2):
         """
