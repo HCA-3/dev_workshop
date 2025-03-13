e3f5892a8618cc318e637cd8b9daa026515de47a
DAVIDHCA3 <dscastelblanco82@ucatolica.edu.co>
2025-03-13 14:48:47 -0500

    arreglo perimetro_pentagono_regular

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -148,7 +148,7 @@ class Geometria:
         Returns:
             float: Perímetro del pentágono regular
         """
-        return 6 * lado
+        return 5 * lado
     
     def area_hexagono_regular(self, lado, apotema):
         """
