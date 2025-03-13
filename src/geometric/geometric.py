7d9aefe8206ae7e68c6b13af1af742f8f4fe7723
DAVIDHCA3 <dscastelblanco82@ucatolica.edu.co>
2025-03-13 15:52:40 -0500

    area_superficie_cilindro

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -247,7 +247,7 @@ class Geometria:
         Returns:
             float: Área de la superficie del cilindro
         """
-        return 2 * math.pi * radio * (radio + altura)
+        return round(2 * math.pi * radio * (radio + altura), 2)
     
     def distancia_entre_puntos(self, x1, y1, x2, y2):
         """
