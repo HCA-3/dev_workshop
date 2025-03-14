9535ba6f24544d51ce72dd6bc632775a5bbf0a32
DAVIDHCA3 <dscastelblanco82@ucatolica.edu.co>
2025-03-13 22:24:28 -0500

    invertir_cadena

--- src/string/strings.py
+++ src/string/strings.py
@@ -27,7 +27,10 @@ class Strings:
         Returns:
             str: Cadena invertida
         """
-        pass
+        invertida = ""
+        for char in texto:
+            invertida = char + invertida
+        return invertida
     
     def contar_vocales(self, texto):
         """
