3c0d6ac0b76fa7e109a8e4f5fefa76892cb5ee18
DAVIDHCA3 <dscastelblanco82@ucatolica.edu.co>
2025-03-13 19:22:37 -0500

    arreglo f13

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -1,4 +1,6 @@
+import pytest
 import math 
+
 class Geometria:
     """
     Class with geometric exercises.
@@ -309,10 +311,10 @@ class Geometria:
         Returns:
             tuple: Coeficientes (A, B, C) de la ecuación de la recta
         """
-        A = y2 - y1
-        B = -(x2 - x1)
-        C = (x2 * y1) - (x1 * y2)
-        return (A, B, C)
+        A = y1 - y2
+        B = x2 - x1
+        C = (x1 * y2) - (x2 * y1)
+        return A, B, C
     
     def area_poligono_regular(self, num_lados, lado, apotema):
         """
