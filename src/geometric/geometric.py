a36bd4f715ad76f4e6ebf50db1aa2041eed704d9
DAVIDHCA3 <dscastelblanco82@ucatolica.edu.co>
2025-03-19 23:48:53 -0500

    arreglo f faltante 11

--- src/geometric/geometric.py
+++ src/geometric/geometric.py
@@ -253,8 +253,7 @@ class Geometria:
         Returns:
             float: Área de la superficie del cilindro
         """
-        resultado = 2 * math.pi * radio * (radio + altura) # esta
-        return round(resultado, 2)
+        return 2 * math.pi * radio * (radio + altura)
     
     def distancia_entre_puntos(self, x1, y1, x2, y2):
         """
@@ -343,11 +342,6 @@ class Geometria:
         """
         if num_lados < 3:
             raise ValueError("El número de lados debe ser al menos 3.")
-    
-    # Excepción para el cuadrado
-        if num_lados == 4:
-            return lado * lado
-    
         return (num_lados * lado * apotema) / 2
     
     def perimetro_poligono_regular(self, num_lados, lado):
